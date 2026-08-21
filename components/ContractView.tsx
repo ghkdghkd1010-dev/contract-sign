1c35cb4ae2cf9d93894875317021b726a84e484c
ghkdghkd1010-dev <ghkdghkd1010@gmail.com>
2026-08-21 19:25:51 +0900

    업데이트 8.21

--- components/ContractView.tsx
+++ components/ContractView.tsx
@@ -629,7 +629,7 @@ export default function ContractView({
                 max-w-none
                 shrink-0
                 object-contain
-                opacity-[0.15]
+                opacity-[0.11]
                 sm:h-[480px]
                 sm:w-[480px]
               "
